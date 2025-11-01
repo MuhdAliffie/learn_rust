ff06cb9ffd68da50dec71f1681156716b982fc77
unknown <aliffieworkonly@gmail.com>
2025-11-01 12:01:31 +0800

    completed stdin

---
+++ std_learn/target/debug/deps/std_learn.d
@@ -0,0 +1,5 @@
+D:\project\Rust\learn\std_learn\target\debug\deps\std_learn.d: src\main.rs
+
+D:\project\Rust\learn\std_learn\target\debug\deps\std_learn.exe: src\main.rs
+
+src\main.rs:
---
+++ std_learn/target/debug/std_learn.d
@@ -0,0 +1,1 @@
+D:\project\Rust\learn\std_learn\target\debug\std_learn.exe: D:\project\Rust\learn\std_learn\src\main.rs
