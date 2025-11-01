ddd3da645c614c56c96b8193ac6d4b54de877679
unknown <aliffieworkonly@gmail.com>
2025-11-01 11:37:36 +0800

    Tried data types and trying git command

---
+++ data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+D:\project\Rust\learn\data_types\target\debug\data_types.exe: D:\project\Rust\learn\data_types\src\main.rs
---
+++ data_types/target/debug/deps/data_types.d
@@ -0,0 +1,5 @@
+D:\project\Rust\learn\data_types\target\debug\deps\data_types.d: src\main.rs
+
+D:\project\Rust\learn\data_types\target\debug\deps\data_types.exe: src\main.rs
+
+src\main.rs:
